ac4ee4b9976a63f8121dddc3a7d4dfd1a8c1b8b4
Vasyl Dembitskyi <vasyl.dembitskyi@outlook.com>
2026-05-23 04:48:29 -0500

    Drop ros cache

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -7,11 +7,9 @@
       experimental-features = "nix-command flakes pipe-operators";
 
       substituters = [
-        "https://ros.cachix.org"
         "https://hyprland.cachix.org"
       ];
       trusted-public-keys = [
-        "ros.cachix.org-1:dSyZxI8geDCJrwgvCOHDoAfOm5sV1wCPjBkKL+38Rvo="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
     };
