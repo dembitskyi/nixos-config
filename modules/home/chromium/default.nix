a3ec593ec1f4deb5af30e3cce68bb47d45b0b27b
Vasyl Dembitskyi <vasyl.dembitskyi@outlook.com>
2026-09-20 19:12:14 -0500

    chromium: add optional local remote debugging

--- modules/home/chromium/default.nix
+++ modules/home/chromium/default.nix
@@ -4,18 +4,42 @@
   pkgs,
   ...
 }:
+let
+  cfg = config.mine.home.chromium;
+in
 {
-
   options = {
-    mine.home.chromium.enable = lib.mkEnableOption "enable chromium browser";
+    mine.home.chromium = {
+      enable = lib.mkEnableOption "enable chromium browser";
+
+      remoteDebugging = {
+        enable = lib.mkEnableOption "Chromium remote debugging for the existing browser profile";
+
+        address = lib.mkOption {
+          type = lib.types.str;
+          default = "127.0.0.1";
+          description = "Address used by Chromium's remote debugging server.";
+        };
+
+        port = lib.mkOption {
+          type = lib.types.port;
+          default = 9224;
+          description = "Port used by Chromium's remote debugging server.";
+        };
+      };
+    };
   };
 
-  config = lib.mkIf config.mine.home.chromium.enable {
+  config = lib.mkIf cfg.enable {
     programs.chromium = {
       enable = true;
       package = pkgs.chromium.override { enableWideVine = true; };
       commandLineArgs = [
         "--enable-features=VaapiVideoDecoder,VaapiVideoEncoder,WaylandWindowDecorations"
+      ]
+      ++ lib.optionals cfg.remoteDebugging.enable [
+        "--remote-debugging-address=${cfg.remoteDebugging.address}"
+        "--remote-debugging-port=${toString cfg.remoteDebugging.port}"
       ];
     };
   };
